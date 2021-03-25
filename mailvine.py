d8e352d92be9a0e87aaed87662c4ef13a97003a9
David Hernandez <dae.aznar@gmail.com>
2021-03-25 17:29:38 -0600

    Minor fixes

--- mailvine.py
+++ mailvine.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_mysqldb import MySQL
+
+# from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
 
 # Index
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 # Login
 @app.route('/login.html')
@@ -14,6 +17,5 @@ def login():
     return render_template('login.html')
 
 
-if __name__ == "__main__":
-    app.run(port=3000, debug=True)
-
+if __name__ == '__main__':
+    app.run(debug=True)
