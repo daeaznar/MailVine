95e80de8079d830206d478c4f4e6ecbaecda529a
David Hernandez <dae.aznar@gmail.com>
2021-03-26 17:29:53 -0600

    Started Login and Registration forms

---
+++ forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Email, EqualTo
+
+
+# Registration form
+class RegistrationForm(FlaskForm):
+    firstName = StringField('FirstName', validators=[DataRequired()])
+    lastName = StringField('LastName', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('ConfirmPassword', validators=[DataRequired(), EqualTo('password')])
+
+    submit = SubmitField('Sign Up')
+
+
+# Login form
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Login')
--- mailvine.py
+++ mailvine.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
+from forms import RegistrationForm, LoginForm
 
 # from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+# Secret Key to protect data. Protection against cookie data tampering.
+app.config['SECRET_KEY'] = '2a44893ff4337692349481e9a9e77e75'
 
 
 # Index
@@ -14,7 +18,15 @@ def index():
 # Login
 @app.route('/login.html')
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    return render_template('login.html', form=form)
+
+
+# User Registration/Sign up
+@app.route('/register.html')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', form=form)
 
 
 if __name__ == '__main__':
